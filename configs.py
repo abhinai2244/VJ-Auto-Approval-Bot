c243bbcae97756ffc0af716f2f2c15b6caf6f7c3
Abhinai Reddy⛔️ <159715958+abhinai2244@users.noreply.github.com>
2025-02-18 13:53:39 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -13,7 +13,7 @@ class Config:
     CHID = int(getenv("CHID", "-1002446190606")) # Make Bot Admin In This Channel
     # Admin Or Owner Id Below
     SUDO = list(map(int, getenv("SUDO", "5756495153").split()))
-    MONGO_URI = getenv("MONGO_URI", "mongodb+srv://user1:abhinai.2244@cluster0.7oaqx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    MONGO_URI = getenv("MONGO_URI", "mongodb+srv://newabhi:newabhi@cluster0.tny8i.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     
 cfg = Config()
 
